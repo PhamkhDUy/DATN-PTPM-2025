a9cfb751fc48e3a64e4f3944ddcdffcca1691cbc
PhamkhDUy <duypkph34320@fpt.edu.vn>
2025-01-26 13:16:30 +0700

    update them

--- app.js
+++ app.js
@@ -6,17 +6,49 @@ app.config(function ($routeProvider) {
       templateUrl: "./views/home.html",
       controller: "HomeController",
     })
-    .when("/about",{
-      templateUrl: "./view/about.html",
-      controller: ""
+    .when("/about", {
+      templateUrl: "./views/about.html", 
+      controller: "AboutController"
+    })
+    .when("/product", {
+      templateUrl: "./views/product.html", 
+      controller: "ProductController"
+    })
+    .when("/product/new",{
+      templateUrl: "./views/product-new.html",
+      controller: "NewController"
+    })
+    .when("/product/featured",{
+      templateUrl: "./views/product-featured.html",
+      controller: "FeaturedController"
+    })
+    .when("/product/promotion",{
+      templateUrl: "./views/product-promotion.html",
+      controller: "PromotionController"
     })
     .otherwise({
       redirectTo: "/",
     });
 });
+app.controller("AboutController", function($scope){
+})
+
+app.controller("ProductController", function ($scope) {
+  $scope.products = products;
+});
+app.controller("NewController", function ($scope) {
+  $scope.products = products;
+});
+app.controller("FeaturedController", function ($scope) {
+  $scope.products = products;
+});
+app.controller("PromotionController", function ($scope) {
+  $scope.products = products;
+});
+
+
 
 app.controller("HomeController", function ($scope) {
-  // Gắn dữ liệu từ product.js vào $scope.products
   $scope.products = products;
 });
 
