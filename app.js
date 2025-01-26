acc2954da14e726c35e27b330f09354d637c6dc0
PhamkhDUy <duypkph34320@fpt.edu.vn>
2025-01-26 15:59:48 +0700

    update them product

--- app.js
+++ app.js
@@ -7,34 +7,48 @@ app.config(function ($routeProvider) {
       controller: "HomeController",
     })
     .when("/about", {
-      templateUrl: "./views/about.html", 
-      controller: "AboutController"
+      templateUrl: "./views/about.html",
+      controller: "AboutController",
     })
     .when("/product", {
-      templateUrl: "./views/product.html", 
-      controller: "ProductController"
+      templateUrl: "./views/product.html",
+      controller: "ProductController",
     })
-    .when("/product/new",{
-      templateUrl: "./views/product-new.html",
-      controller: "NewController"
+    .when("/product/new", {
+      templateUrl: "./views/product/product-new.html",
+      controller: "NewController",
     })
-    .when("/product/featured",{
-      templateUrl: "./views/product-featured.html",
-      controller: "FeaturedController"
+    .when("/product/featured", {
+      templateUrl: "./views/product/product-featured.html",
+      controller: "FeaturedController",
     })
-    .when("/product/promotion",{
-      templateUrl: "./views/product-promotion.html",
-      controller: "PromotionController"
+    .when("/product/promotion", {
+      templateUrl: "./views/product/product-promotion.html",
+      controller: "PromotionController",
+    })
+    .when("/product/id/:productId", {
+      templateUrl: "./views/product/detailProduct.html",
+      controller: "DetailProductController",
     })
     .otherwise({
       redirectTo: "/",
     });
 });
-app.controller("AboutController", function($scope){
-})
+app.controller("AboutController", function ($scope) {});
 
 app.controller("ProductController", function ($scope) {
   $scope.products = products;
+  $scope.quantity = 1;
+
+  $scope.increment = function () {
+    $scope.quantity += 1;
+  };
+
+  $scope.decrement = function () {
+    if ($scope.quantity > 1) {
+      $scope.quantity -= 1;
+    }
+  };
 });
 app.controller("NewController", function ($scope) {
   $scope.products = products;
@@ -46,9 +60,30 @@ app.controller("PromotionController", function ($scope) {
   $scope.products = products;
 });
 
+app.controller("DetailProductController", function ($scope, $routeParams) {
+  const productId = parseInt($routeParams.productId);
+  $scope.products = products;
 
+  $scope.selectedProduct   = products.find(function (product) {
+    return product.id === productId;
+  });
+  $scope.quantity = 1;
+  $scope.increment = function () {
+    $scope.quantity += 1;
+  };
+  $scope.decrement = function () {
+    if ($scope.quantity > 1) {
+      $scope.quantity -= 1;
+    }
+  };
+
+  $scope.validateQuantity = function () {
+    if ($scope.quantity < 1 || isNaN($scope.quantity)) {
+      $scope.quantity = 1;
+    }
+  };
+});
 
 app.controller("HomeController", function ($scope) {
   $scope.products = products;
 });
-
--- assets/fake-data/products.js
+++ assets/fake-data/products.js
@@ -4,96 +4,128 @@ const products = [
     title: "Giày thể thao nam",
     price: 500000,
     image: "assets/images/giay1.jpg",
-    category: "New || all",
+    category: "New",
     description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
   },
   {
     id: 2,
     title: "Giày chạy bộ",
     price: 700000,
     image: "assets/images/giay2.jpg",
-    category: "Featured || all",
+    category: "Featured",
     description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 3,
     title: "Giày đá bóng",
     price: 800000,
     image: "assets/images/giay3.jpg",
-    category: "New || all",
+    category: "New ",
     description: "Giày đá bóng chuyên nghiệp.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
   },
   {
     id: 4,
     title: "Giày sneaker",
     price: 1200000,
     image: "assets/images/giay4.jpg",
-    category: "Featured || all",
+    category: "Featured ",
     description: "Giày sneaker thời trang.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 5,
     title: "Giày lười nam",
     price: 600000,
     image: "assets/images/giay5.jpg",
-    category: "New || all",
+    category: "New ",
     description: "Giày lười nam thoải mái.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 6,
     title: "Giày cao gót nữ",
     price: 900000,
     image: "assets/images/giay6.jpg",
-    category: "New || all",
+    category: "New ",
     description: "Giày cao gót nữ thanh lịch.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 7,
     title: "Giày boot nữ",
     price: 1100000,
     image: "assets/images/giay7.webp",
-    category: "Featured || all",
+    category: "Featured ",
     description: "Giày boot nữ phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 8,
     title: "Giày cổ cao nữ",
     price: 1100000,
     image: "assets/images/giay8.jpg",
-    category: "Featured || all",
+    category: "Featured ",
     description: "Giày cổ cao nữ phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 9,
     title: "Giày cao gót nữ",
     price: 1100000,
     image: "assets/images/giay9.jpg",
-    category: " Promotion || all",
+    category: " Promotion ",
     description: "Giày cao gót nữ phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 10,
     title: "Giày lười",
     price: 1100000,
     image: "assets/images/giay10.jpg",
-    category: "Promotion || all",
+    category: "Promotion ",
     description: "Giày lười phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 11,
     title: "Giày cao gót nữ",
     price: 1100000,
     image: "assets/images/giay9.jpg",
-    category: " Promotion || all",
+    category: " Promotion ",
     description: "Giày cao gót nữ phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
   },
   {
     id: 12,
     title: "Giày lười",
     price: 1100000,
     image: "assets/images/giay10.jpg",
-    category: "Promotion || all",
+    category: "Promotion ",
     description: "Giày lười phong cách.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
   },
 ];
-console.log(products.image)
