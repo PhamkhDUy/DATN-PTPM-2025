7efd8edead971fdba84e05f3dca8cd35b7ce9c47
PhamkhDUy <duypkph34320@fpt.edu.vn>
2025-01-29 03:41:34 +0700

    update 29/1/2025/

--- app.js
+++ app.js
@@ -34,37 +34,170 @@ app.config(function ($routeProvider) {
       redirectTo: "/",
     });
 });
+function setActiveLink() {
+  var navLinks = document.querySelectorAll("#nav-bar a");
+  navLinks.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  var currentHash = window.location.hash;
+  console.log("Current Hash:", currentHash);
+  navLinks.forEach(function (link) {
+    var linkHref = link.getAttribute("href");
+    if (currentHash === linkHref || currentHash.startsWith(linkHref + "/")) {
+      link.classList.add("active");
+    }
+  });
+}
+window.addEventListener("load", setActiveLink);
+window.addEventListener("hashchange", setActiveLink);
+
+function setActiveCategory() {
+  var categoryLinks = document.querySelectorAll("#ul-bar a");
+  categoryLinks.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  var currentHash = window.location.hash;
+  categoryLinks.forEach(function (link) {
+    var linkHref = link.getAttribute("href");
+
+    if (currentHash === linkHref) {
+      link.classList.add("active");
+    }
+  });
+}
+window.addEventListener("load", setActiveCategory);
+window.addEventListener("hashchange", setActiveCategory);
+
 app.controller("AboutController", function ($scope) {});
-
-app.controller("ProductController", function ($scope) {
+app.controller("ProductController", function ($scope, $location, $document) {
   $scope.products = products;
-  $scope.quantity = 1;
+  $scope.itemsPerPage = 12;
+  $scope.currentPage = 1;
+  $scope.totalPages = Math.ceil($scope.products.length / $scope.itemsPerPage); // Total number of pages
+  $scope.isHovering = false;
 
-  $scope.increment = function () {
-    $scope.quantity += 1;
+  $scope.updatePaginatedProducts = function () {
+    const startIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProducts = $scope.products.slice(startIndex, endIndex);
+  };
+  $scope.goToDetailPage = function (productId) {
+    $location.path("/product/id/" + productId);
   };
 
-  $scope.decrement = function () {
-    if ($scope.quantity > 1) {
-      $scope.quantity -= 1;
+  $document.on("click", function (event) {
+    const target = event.target;
+    const hoverButtons = document.querySelectorAll(".hover-overlay button");
+    const isClickInsideButton = Array.from(hoverButtons).some((button) =>
+      button.contains(target)
+    );
+    if (!isClickInsideButton) {
+      const productId = target
+        .closest(".product-cart")
+        ?.querySelector("a")
+        ?.getAttribute("href")
+        ?.split("/")
+        .pop();
+      if (productId) {
+        $scope.goToDetailPage(productId);
+      }
+    }
+  });
+
+  $scope.goToPage = function (page) {
+    if (page > 0 && page <= $scope.totalPages) {
+      $scope.currentPage = page;
+      $scope.updatePaginatedProducts();
     }
   };
-});
-app.controller("NewController", function ($scope) {
-  $scope.products = products;
-});
-app.controller("FeaturedController", function ($scope) {
-  $scope.products = products;
-});
-app.controller("PromotionController", function ($scope) {
-  $scope.products = products;
+
+  $scope.updatePaginatedProducts();
 });
 
+app.controller("NewController", function ($scope) {
+  $scope.products = products;
+  $scope.filteredProducts = $scope.products.filter(function (product) {
+    return product.category === "New";
+  });
+  $scope.itemsPerPage = 12;
+  $scope.currentPage = 1;
+  $scope.updatePaginatedProducts = function () {
+    const startIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProducts = $scope.filteredProducts.slice(
+      startIndex,
+      endIndex
+    );
+  };
+  $scope.goToPage = function (page) {
+    if (page > 0 && page <= $scope.totalPages) {
+      $scope.currentPage = page;
+      $scope.updatePaginatedProducts();
+    }
+  };
+  $scope.totalPages = Math.ceil(
+    $scope.filteredProducts.length / $scope.itemsPerPage
+  );
+  $scope.updatePaginatedProducts();
+});
+
+app.controller("FeaturedController", function ($scope) {
+  $scope.products = products;
+  $scope.filteredProducts = $scope.products.filter(function (product) {
+    return product.category === "Featured";
+  });
+  $scope.itemsPerPage = 12;
+  $scope.currentPage = 1;
+  $scope.updatePaginatedProducts = function () {
+    const startIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProducts = $scope.filteredProducts.slice(
+      startIndex,
+      endIndex
+    );
+  };
+  $scope.goToPage = function (page) {
+    if (page > 0 && page <= $scope.totalPages) {
+      $scope.currentPage = page;
+      $scope.updatePaginatedProducts();
+    }
+  };
+  $scope.totalPages = Math.ceil(
+    $scope.filteredProducts.length / $scope.itemsPerPage
+  );
+  $scope.updatePaginatedProducts();
+});
+
+app.controller("PromotionController", function ($scope) {
+  $scope.products = products;
+  $scope.filteredProducts = $scope.products.filter(function (product) {
+    return product.category === "Promotion";
+  });
+  $scope.itemsPerPage = 12;
+  $scope.currentPage = 1;
+  $scope.updatePaginatedProducts = function () {
+    const startIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProducts = $scope.filteredProducts.slice(
+      startIndex,
+      endIndex
+    );
+  };
+  $scope.goToPage = function (page) {
+    if (page > 0 && page <= $scope.totalPages) {
+      $scope.currentPage = page;
+      $scope.updatePaginatedProducts();
+    }
+  };
+  $scope.totalPages = Math.ceil(
+    $scope.filteredProducts.length / $scope.itemsPerPage
+  );
+  $scope.updatePaginatedProducts();
+});
 app.controller("DetailProductController", function ($scope, $routeParams) {
   const productId = parseInt($routeParams.productId);
   $scope.products = products;
-
-  $scope.selectedProduct   = products.find(function (product) {
+  $scope.selectedProduct = products.find(function (product) {
     return product.id === productId;
   });
   $scope.quantity = 1;
@@ -83,7 +216,51 @@ app.controller("DetailProductController", function ($scope, $routeParams) {
     }
   };
 });
-
 app.controller("HomeController", function ($scope) {
-  $scope.products = products;
+  $scope.productsNew = products.filter(
+    (product) => product.category.trim() === "New"
+  );
+  $scope.productsFeatured = products.filter(
+    (product) => product.category.trim() === "Featured"
+  );
+  $scope.itemsPerPage = 4;
+  $scope.currentPageNew = 1;
+  $scope.totalPagesNew = Math.ceil(
+    $scope.productsNew.length / $scope.itemsPerPage
+  );
+  $scope.totalPagesFeatured = Math.ceil(
+    $scope.productsFeatured.length / $scope.itemsPerPage
+  );
+  $scope.currentPageFeatured = 1;
+  $scope.totalPagesFeatured = Math.ceil(
+    $scope.productsFeatured.length / $scope.itemsPerPage
+  );
+  $scope.updatePaginatedProductsNew = function () {
+    const startIndex = ($scope.currentPageNew - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProductsNew = $scope.productsNew.slice(
+      startIndex,
+      endIndex
+    );
+  };
+
+  $scope.updatePaginatedProductsFeatured = function () {
+    const startIndex = ($scope.currentPageFeatured - 1) * $scope.itemsPerPage;
+    const endIndex = startIndex + $scope.itemsPerPage;
+    $scope.paginatedProductsFeatured = $scope.productsFeatured.slice(
+      startIndex,
+      endIndex
+    );
+  };
+  $scope.goToPageNew = function (page) {
+    $scope.currentPageNew = page;
+    $scope.updatePaginatedProductsNew();
+  };
+
+  $scope.goToPageFeatured = function (page) {
+    $scope.currentPageFeatured = page;
+    $scope.updatePaginatedProductsFeatured();
+  };
+  $scope.updatePaginatedProductsNew();
+  $scope.updatePaginatedProductsFeatured();
 });
--- assets/fake-data/products.js
+++ assets/fake-data/products.js
@@ -10,7 +10,175 @@ const products = [
     trangThai: "Còn hàng"
   },
   {
+    id: 111,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay1.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 112,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay1.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 113,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay1.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 114,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay1.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 115,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay1.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 116,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay2.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 117,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay2.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
+    id: 118,
+    title: "Giày thể thao nam",
+    price: 500000,
+    image: "assets/images/giay2.jpg",
+    category: "New",
+    description: "Giày thể thao nam chất lượng cao.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+  },
+  {
     id: 2,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 222,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 223,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 224,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 225,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 226,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 227,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 228,
+    title: "Giày chạy bộ",
+    price: 700000,
+    image: "assets/images/giay2.jpg",
+    category: "Featured",
+    description: "Giày chạy bộ nhẹ, bền bỉ.",
+    soLuong: 110,
+    trangThai: "Còn hàng"
+
+  },
+  {
+    id: 229,
     title: "Giày chạy bộ",
     price: 700000,
     image: "assets/images/giay2.jpg",
@@ -101,7 +269,7 @@ const products = [
     title: "Giày lười",
     price: 1100000,
     image: "assets/images/giay10.jpg",
-    category: "Promotion ",
+    category: "Promotion",
     description: "Giày lười phong cách.",
     soLuong: 110,
     trangThai: "Còn hàng"
@@ -112,7 +280,7 @@ const products = [
     title: "Giày cao gót nữ",
     price: 1100000,
     image: "assets/images/giay9.jpg",
-    category: " Promotion ",
+    category: "Promotion",
     description: "Giày cao gót nữ phong cách.",
     soLuong: 110,
     trangThai: "Còn hàng"
@@ -123,7 +291,7 @@ const products = [
     title: "Giày lười",
     price: 1100000,
     image: "assets/images/giay10.jpg",
-    category: "Promotion ",
+    category: "Promotion",
     description: "Giày lười phong cách.",
     soLuong: 110,
     trangThai: "Còn hàng"
